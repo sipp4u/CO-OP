663c011d63e433b1f8ef7d563d118a57f789a2b0
Matmat Myriam <matmat.myriam@gmail.com>
2018-01-08 15:39:55 +0100

    init form to sign in

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -52,9 +52,18 @@ input[type=text], select {
   border-radius: 4px;
   box-sizing: border-box;
 }
+input[type=password], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
 
 input[type=submit] {
-  width: 100%;
+  width:50%;
   background-color: #4CAF50;
   color: white;
   padding: 14px 20px;
